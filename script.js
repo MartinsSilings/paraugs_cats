4e22da1e0d9da5ebe0d68b2ee59bec154debbe3f
MartinsSilings <112856465+MartinsSilings@users.noreply.github.com>
2024-04-15 15:40:06 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log(1)
+function sutitzinu()
+{
+    console.log('sutitzinu()darbojas')
+    let zina = document.querySelector('zina');
+    let zinas = document.querySelector('catazina');
+    zina.innerHTML = zinas.innerHTML+ '<br>'+zina.value;
+}
